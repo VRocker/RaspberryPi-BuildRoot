493b20de596c15672d90157d588f9a8db3997cd9
VRocker <vrocker@gmail.com>
2012-12-06 12:20:29 +0000

    Fixed the busybox config, Fixed the inittab, Replaced bash with ash

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -3,14 +3,15 @@ TARGETDIR=$1
 # mkpasswd, from the 'whois' package in Debian/Ubuntu.
 sed -i 's%^root::%root:8kfIfYHmcyQEE:%' $TARGETDIR/etc/shadow
 
-# Point /bin/sh to /bin/bash
-ln -T -s /bin/bash $TARGETDIR/bin/sh 
+# Point /bin/sh to /bin/ash
+# We want to use ash as its more lightweight than bash
+ln -T -s busybox $TARGETDIR/bin/sh
 
 # Package the /boot partition
 tar -czf $TARGETDIR/../images/boot.tar.gz --exclude=Image -C $TARGETDIR/boot/ .
 
 # add a corrected, and lightweight inittab
-cp board/raspberrypi/inittab $TARGETDIR/etc/inittab
+# cp board/raspberrypi/inittab $TARGETDIR/etc/inittab
 
 # add eth0 dhcp entry into /etc/network/interfaces
 cp board/raspberrypi/interfaces $TARGETDIR/etc/network/
